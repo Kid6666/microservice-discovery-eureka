dfe323850a3fd0cb86467333fd058b17e02d08d0
kid6666 <1012468713@qq.com>
2019-10-14 01:18:23 +0800

    first commited

---
+++ src/test/java/com/lejx/microservicediscoveryeureka/MicroserviceDiscoveryEurekaApplicationTests.java
@@ -0,0 +1,16 @@
+package com.lejx.microservicediscoveryeureka;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MicroserviceDiscoveryEurekaApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
